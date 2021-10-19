4fc37a9b5c0a53425a4014191f73f5530f8eec5d
jagdishlove <jagdish.funn@gmail.com>
2021-10-20 01:21:56 +0530

    added-deleteTodo

--- server/resolvers.js
+++ server/resolvers.js
@@ -8,9 +8,25 @@ const resolvers = {
         getTodos: async () => {
             const todos = await Todo.find()
             return todos;
+        },
+        getTodo: async (root, args) => {
+            const todo = await Todo.findById(args.id)
+            return todo;
         }
 
+    }, Mutation: {
+        addTodo: async (root, args) => {
+            const newTodo = new Todo({ title: args.title, detail: args.detail, date: args.date })
+            await newTodo.save()
+            return newTodo
+        },
+        deleteTodo: async (root, args) => {
+            await Todo.findByIdAndDelete(args.id);
+            return "Todo Deleted";
+
+        }
     }
 }
 
-export default resolvers;
+
+export default resolvers
--- server/typeDefs.js
+++ server/typeDefs.js
@@ -6,13 +6,19 @@ scalar Date
 type Todo{
     id:ID
     title:String
-    details:String
+    detail:String
     date:Date
 }
 
 type Query{
     welcome:String
     getTodos:[Todo]
+    getTodo(id:ID):Todo
+}
+
+type Mutation{
+    addTodo(title:String, detail:String, date:Date):Todo
+    deleteTodo(id:ID):String
 }
 `
 
